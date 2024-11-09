7ae4131aeec7ae13413ee2f1c187059ccdf2f0e8
Denis <denismaingi49@gmail.com>
2024-11-10 00:35:52 +0300

    Fuck you 2

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ jwt = JWTManager(app)
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=10)  # Access token expires in 10 hours
 JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 
-UPLOAD_FOLDER = 'Image'  # Relative path or absolute path
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Image')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
